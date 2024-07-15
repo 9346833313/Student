8456c79701074df79f8ab4578a681b5aa34408cc
NukathotiVamsi <vamsinukathoti224@gmail.com>
2024-07-15 17:55:22 +0530

    Changes the LoggingAspect

--- src/main/java/com/example/student/aspect/LoggingAspect.java
+++ src/main/java/com/example/student/aspect/LoggingAspect.java
@@ -1,63 +1,43 @@
 package com.example.student.aspect;
 
+import java.util.Arrays;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import com.example.student.entity.Faculty;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private static final Logger logger = LogManager.getLogger(LoggingAspect.class);
 
-	@Pointcut("execution(* com.example.springboot.controller.FacultyController.*(..))")
+	@Pointcut("execution(* com.example.student.controller.*.*(..))")
 	public void loggingPointCut() {
-		// Pointcut for all methods in FacultyController
 	}
 
 	@Around("loggingPointCut()")
 	public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-		logger.info("Before method invoked: {}", joinPoint.getSignature());
-		logger.info("Request: {}", getRequestDetails(joinPoint.getArgs()));
+		logger.info("Before method invoked: " + joinPoint.getSignature());
+		logger.info("Request: " + Arrays.toString(joinPoint.getArgs()));
 
 		Object result = joinPoint.proceed();
 
-		logger.info("After method invoked: {}", joinPoint.getSignature());
+		logger.info("After method invoked: " + joinPoint.getSignature());
+		logger.info("Response: " + result);
 
 		return result;
 	}
 
-	@AfterReturning(pointcut = "loggingPointCut()", returning = "result")
-	public void afterReturning(JoinPoint joinPoint, Object result) {
-		logger.info("Response: {}", getFacultyResponse(result));
-	}
-
-	private String getRequestDetails(Object[] args) {
-		if (args != null && args.length > 0) {
-			for (Object arg : args) {
-				if (arg instanceof Faculty) {
-					Faculty faculty = (Faculty) arg;
-					return getFacultyResponse(faculty);
-				}
-			}
-		}
-		return "No Faculty data in the request";
-	}
-
-	private String getFacultyResponse(Object result) {
-		if (result instanceof Faculty) {
-			Faculty faculty = (Faculty) result;
-			return "Faculty [fname=" + faculty.getFname() + ", fsubject=" + faculty.getFsubject() + ", description="
-					+ faculty.getDescription() + "]";
-		}
-		return "No Faculty data in the response";
+	@AfterThrowing(pointcut = "loggingPointCut()", throwing = "exception")
+	public void afterThrowing(JoinPoint joinPoint, Throwable exception) {
+		logger.info("After method invoked: " + joinPoint.getSignature());
+		logger.info("Method threw exception: " + exception.getMessage());
 	}
 }
