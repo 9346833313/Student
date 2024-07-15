5e38a7dc1098f8d60417182276111e24c9ada5fa
sreekanthvellulli <sreekanth.vellulli@spsoft.in>
2024-07-15 12:35:53 +0530

    code changed

--- src/main/java/com/example/student/service/StudentService.java
+++ src/main/java/com/example/student/service/StudentService.java
@@ -5,15 +5,7 @@ import org.springframework.http.ResponseEntity;
 import com.example.student.entity.Student;
 
 public interface StudentService {
-<<<<<<< HEAD
-	public ResponseEntity<?> createStudent(Student student);
 
-	public ResponseEntity<?> getStudent();
-	public ResponseEntity<?> getById(long id);
-
-	public ResponseEntity<?> updateUser(Student student, long id);
-}
-=======
 
 	public ResponseEntity<?> createStudent(Student student);
 
@@ -27,4 +19,3 @@ public interface StudentService {
 	public ResponseEntity<?> delete(long id);
 	}
 
->>>>>>> 2ba2b6255f33d33f92e6533a6ed0e7698c6dfcd4
