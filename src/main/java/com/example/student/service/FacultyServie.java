32ca1f53dabafbf52f7d4987ae0db406d8160afb
NukathotiVamsi <vamsinukathoti224@gmail.com>
2024-07-15 11:18:50 +0530

    Code Changes

--- src/main/java/com/example/student/service/FacultyServie.java
+++ src/main/java/com/example/student/service/FacultyServie.java
@@ -1,5 +1,15 @@
 package com.example.student.service;
 
+import java.util.List;
+
+import com.example.student.entity.Faculty;
+
 public interface FacultyServie {
+
+	public List<Faculty> getAllFaculty();
+
+	public Faculty getFacultyById(long id);
+
+	public void deleteFacultyById(long id);
 
 }
--- src/main/java/com/example/student/serviceimpl/FacultyServiceImpl.java
+++ src/main/java/com/example/student/serviceimpl/FacultyServiceImpl.java
@@ -1,10 +1,36 @@
 package com.example.student.serviceimpl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.example.student.entity.Faculty;
+import com.example.student.repository.FacultyRepository;
 import com.example.student.service.FacultyServie;
 
 @Service
-public class FacultyServiceImpl implements FacultyServie{
+public class FacultyServiceImpl implements FacultyServie {
+	@Autowired
+	private FacultyRepository facultyRepository;
+
+	@Override
+	public List<Faculty> getAllFaculty() {
+		return facultyRepository.findAll();
+	}
+
+	@Override
+	public Faculty getFacultyById(long id) {
+		Optional<Faculty> optionalFaculty = facultyRepository.findById(id);
+		return optionalFaculty.get();
+	}
+
+	@Override
+	public void deleteFacultyById(long id) {
+		// TODO Auto-generated method stub
+		facultyRepository.deleteById(id);
+	}
 
 }
