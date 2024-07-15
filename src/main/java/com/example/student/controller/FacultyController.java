438d29de0e975d2422b1bd86eded85c52cbac2c2
bharathidamarla <bharathi.devi@spsoft.in>
2024-07-15 10:58:07 +0530

    Adding Faculty

---
+++ src/main/java/com/example/student/controller/FacultyController.java
@@ -0,0 +1,10 @@
+package com.example.student.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Faculty")
+public class FacultyController {
+
+}
---
+++ src/main/java/com/example/student/serviceimpl/FacultyServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.student.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.student.service.FacultyServie;
+
+@Service
+public class FacultyServiceImpl implements FacultyServie{
+
+}
