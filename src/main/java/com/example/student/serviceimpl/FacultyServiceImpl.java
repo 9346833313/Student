390a5d07fc56afd0b5338a627f5e887725394a67
NukathotiVamsi <vamsinukathoti224@gmail.com>
2024-07-15 16:43:48 +0530

    Changes in FcaultyService

--- src/main/java/com/example/student/serviceimpl/FacultyServiceImpl.java
+++ src/main/java/com/example/student/serviceimpl/FacultyServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.student.serviceimpl;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.student.Dto.FacultyReqDto;
+import com.example.student.ResDto.ResponseDto;
+import com.example.student.ResDto.ResponseMessage;
 import com.example.student.entity.Faculty;
 import com.example.student.repository.FacultyRepository;
 import com.example.student.service.FacultyService;
@@ -18,6 +19,7 @@ public class FacultyServiceImpl implements FacultyService {
 
 	@Autowired
 	private FacultyRepository facultyRepository;
+	
 
 	@Override
 	public ResponseEntity<?> createFaculty(FacultyReqDto facultyReqDto) {
@@ -34,14 +36,35 @@ public class FacultyServiceImpl implements FacultyService {
 	}
 
 	@Override
-	public Faculty getFacultyById(long id) {
-		Optional<Faculty> optionalFaculty = facultyRepository.findById(id);
-		return optionalFaculty.get();
+	public ResponseEntity<?> findFacultyById(Long id) {
+		ResponseDto response = new ResponseDto();
+
+		try {
+			Optional<Faculty> faculty = facultyRepository.findById(id);
+			if (faculty.isPresent()) {
+
+				response.setStatusCode(200);
+				response.setIsError(false);
+				response.setResult("");
+				return new ResponseEntity<>(response, HttpStatus.OK);
+			} else {
+				response.setStatusCode(400);
+				response.setIsError(true);
+				response.setResult(new ResponseMessage("User not found with id: " + id));
+				return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			response.setStatusCode(500);
+			response.setIsError(true);
+			response.setResult(new ResponseMessage("An unexpected error occurred. Please try again later."));
+			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 	@Override
-	public List<Faculty> getAllFaculty() {
-		return facultyRepository.findAll();
+	public ResponseEntity<?> getAllFaculty() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
@@ -62,8 +85,21 @@ public class FacultyServiceImpl implements FacultyService {
 	}
 
 	@Override
-	public void deleteFacultyById(long id) {
-		facultyRepository.deleteById(id);
+	public ResponseEntity<ResponseDto> deleteFacultyId(long facultyId) {
+		ResponseDto response = new ResponseDto();
+		Optional<Faculty> faculty = facultyRepository.findById(facultyId);
+		if (faculty.isPresent()) {
+			facultyRepository.deleteById(facultyId);
+			response.setStatusCode(200);
+			response.setIsError(false);
+			response.setResult(new ResponseMessage("Faculty Id deleted successfully"));
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} else {
+			response.setStatusCode(400);
+			response.setIsError(true);
+			response.setResult(new ResponseMessage("Faculty not found with id: " + facultyId));
+			return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
