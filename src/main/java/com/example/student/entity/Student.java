f5f16641765c11e26988b1921e178d55efc7860f
sreekanthvellulli <sreekanth.vellulli@spsoft.in>
2024-07-15 11:00:43 +0530

    added entity,repo

--- src/main/java/com/example/student/entity/Student.java
+++ src/main/java/com/example/student/entity/Student.java
@@ -1,5 +1,51 @@
 package com.example.student.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	private long rollNo;
+
+	public Student() {
+		super();
+	}
+
+	public Student(long id, String name, long rollNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.rollNo = rollNo;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(long rollNo) {
+		this.rollNo = rollNo;
+	}
 
 }
